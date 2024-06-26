460163030c7731376f293768eedb0509b050a0b2
Dinesh-Rodriguez <antondinesh2021@gmail.com>
2024-06-27 04:57:43 +0530

    CRUD Full

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Room 
+from .models import Room, Topic
 from .forms import RoomForm
 
 #rooms= [
@@ -20,14 +20,38 @@ def room(request, pk):
     context = {'room': room}
     return render(request, 'base/room.html', context)
      
-def CreateRoom(request):
-    print(request.POST) 
+def createRoom(request):
     form = RoomForm()
-    if request.method == 'POST':
+    if request.method =='POST':
         form = RoomForm(request.POST)
+        if form.is_valid():
+           room = form.save(commit=False)
+           room.host = request.user
+           room.save()
+           return redirect('home')
+         
+
+    context={'form':form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
         if form.is_valid():
             form.save()
             return redirect('home')
+            
     
-    context = {'form': form}
+    context = {'form' : form }
     return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk) :
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render (request, 'base/delete.html', {'obj': room})
+
