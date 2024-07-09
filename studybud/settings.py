6974a3ff42838dbe95851f74dd7a8c7c76a5580b
Dinesh-Rodriguez <antondinesh2021@gmail.com>
2024-07-10 05:20:15 +0530

    API Serialization

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base'
+    'base.apps.BaseConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
